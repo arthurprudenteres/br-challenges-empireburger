f573e98657cbec985bdb8abe9ffe1d1b346b94de
Arthur Prudente <arthurpru@gmail.com>
2022-09-23 18:12:43 -0300

    javascript features

--- script/script.js
+++ script/script.js
@@ -1,42 +1,45 @@
-const selectPrice = document.querySelectorAll(".price");
-const selectPlate = document.querySelectorAll(".plate");
-const selectIngredients = document.querySelectorAll(".menu__description");
+function menu() {
+  const selectPrice = document.querySelectorAll(".price");  
+  const selectPlate = document.querySelectorAll(".plate");
+  const selectIngredients = document.querySelectorAll(".menu__description");
 
-fetch('https://api.brchallenges.com/api/empire-burger/menu')
-.then(response => response.json())
-.then(data => {
-    
-  const formatted = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    });
-    
-  const prices = data.map(menu => {
-    return menu.price;
-  })
-  const newPrice = prices.map(price => {
-    return formatted.format(price)
-  })
+  fetch('https://api.brchallenges.com/api/empire-burger/menu')
+  .then(response => response.json())
+  .then(data => {
 
-  const plates = data.map(menu => {
-    return menu.plate;
-  })
-  
-  const ingredients = data.map(menu => {
-    return menu.ingredients;
-  })
-
-  function setData(data, selector) {
-    data.forEach((plates, index) => {
-      selector[index].textContent = `${plates}`
+    const formatted = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      });
+      
+    const prices = data.map(menu => {
+      return menu.price;
     })
-  }
+    const newPrice = prices.map(price => {
+      return formatted.format(price)
+    })
 
-  setData(newPrice, selectPrice);
-  setData(plates, selectPlate);
-  setData(ingredients, selectIngredients)
+    const plates = data.map(menu => {
+      return menu.plate;
+    })
+    
+    const ingredients = data.map(menu => {
+      return menu.ingredients;
+    })
+
+    function setData(data, selector) {
+      data.forEach((plates, index) => {
+        selector[index].textContent = `${plates}`
+      })
+    }
+
+    setData(newPrice, selectPrice);
+    setData(plates, selectPlate);
+    setData(ingredients, selectIngredients)
 })
-  
+
+}
+
 function changeColor() {
   const boxColor = document.getElementById("times");
   const date = new Date();
@@ -54,4 +57,62 @@ function changeColor() {
   }
 }
 
+function data(){
+  const selectFeedback = document.querySelectorAll(".feedback");  
+  const selectName = document.querySelectorAll(".person-name");
+  const selectAge = document.querySelectorAll(".person-age");
+  const selectImage = document.querySelectorAll(".person-img");
+  
+  fetch('https://api.brchallenges.com/api/empire-burger/testimonials')
+  .then(response => response.json())
+  .then(data => {
+  
+      
+    const feedbacks = data.map(info => {
+      return info.testimonial;
+    })
+  
+    const name = data.map(info => {
+      return info.name;
+    })
+    
+    const age = data.map(info => {
+      return info.age;
+    })
+  
+    const image = data.map(info => {
+      return info.image;
+    })
+  
+    function setData(data, selector) {
+      data.forEach((info, index) => {
+        selector[index].textContent = `${info}`
+      })
+    }
+  
+    function Image(data, selector) {
+      data.forEach((plates, index) => {
+        selector[index].innerHTML = `<img src=${plates}>`
+      })
+    }
+  
+    Image(image, selectImage);
+    setData(feedbacks, selectFeedback);
+    setData(name, selectName);
+    setData(age, selectAge)
+  })
+}
+
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  draggable: true,
+  dragVelocity: 0.8,
+  duration: 3,
+  dots: '.carousel-bars',
+},
+);
+
+data();
+menu();
 changeColor();
